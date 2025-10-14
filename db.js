6fd287880e281e04031143a6a6b4f21fa2507914
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-14 11:29:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const mongoURI = process.env.MONGO_URI;
 const connTOMongo = async () => {
    try {
           await mongoose.connect(process.env.MONGO_URI, {
