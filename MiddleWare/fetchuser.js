8bc538303a8228326fbbb7f5111a47a34e19a5f6
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-15 11:53:39 +0530

    Update fetchuser.js

--- MiddleWare/fetchuser.js
+++ MiddleWare/fetchuser.js
@@ -6,7 +6,6 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const fetchuser = (req, res, next) => {
     //get the user from the jwt token and add Id to req object
     const token = req.header("auth-token");
-    console.log("token",token);
     if (!token) {
        return res.status(401).send({ error: "Please authenticate using a valid token" })
     }
